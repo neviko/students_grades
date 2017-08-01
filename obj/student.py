30cada744b74d313e0c0f5e981442be67cb0cd8e
neviko <nevosayag@gmail.com>
2017-08-01 12:42:39 +0300

    xlsx files update and grades calculation

--- obj/student.py
+++ obj/student.py
@@ -1,18 +1,28 @@
+from students_grades.obj import course
+
+
 class Student():
-    def __init__(self, student_data_list):
+    def __init__(self, student_data_list,numOfRequiredEx,weight_final_project):
         self.id = student_data_list[0]
         assert type(self.id) is int
 
         self.name = student_data_list[1]
         self.last_name = student_data_list[2]
 
-        self.ex_grade_list = student_data_list[3:]
+        self.ex_grade_list = student_data_list[3:len(student_data_list)]
         self.ex_grade_list = [grade if grade else 0 for grade in self.ex_grade_list]
         assert all(type(ex) is int for ex in self.ex_grade_list)
+
+
 
         self.exemption_list = []
         self.final_project_grade = None
         self.final_grade = None
+
+        self.numOfRequiredEx = numOfRequiredEx
+        self.weight_final_project = weight_final_project
+
+
 
     def set_exemption(self, exemp):
         # Range
@@ -35,6 +45,27 @@ class Student():
             assert type(exemp) is int
             self.exemption_list.append(exemp)
 
+
+
+
+
+    # def calculateCourseGrade(self):
+    #
+    #     #get exercise average
+    #     exGradesList = reversed(self.ex_grade_list.sort()) #sort grades from high to low
+    #     highestGrades = exGradesList[0:self.numOfRequiredEx] #highest required grades
+    #     exAvg = sum(highestGrades) / float(len(highestGrades))
+    #
+    #     finalGrade =(1- course.weight_final_project)*exAvg + course.weight_final_project * self.student_data_list[-1]
+
+
+
+
+
+
+
+
+
     def print_student(self):
-        print('{}, {}, {}\n{}\nExemptions: {}\n'
-              .format(self.id, self.name, self.last_name, self.ex_grade_list, self.exemption_list))
+        print('{}, {}, {}\n{}\nExemptions: {}\nFinal grade is: {}\n'
+              .format(self.id, self.name, self.last_name, self.ex_grade_list, self.exemption_list,self.final_grade))
--- student_grade.py
+++ student_grade.py
@@ -3,26 +3,54 @@ from openpyxl import load_workbook
 from students_grades.obj.student import Student
 from students_grades.obj.course import Course
 
+
+gradesFilePath = "C:\College Programing\Python\Final project\students_grades\grades.XLSX"
+exemptionsFilePath = "C:\College Programing\Python\Final project\students_grades\exemption.XLSX"
+lectureSheetPath = "C:\College Programing\Python\Final project\students_grades\lectureSheet.XLSX"
+
+weight_final_project=0.0
+numOfRequiredEx=0
+
+
 def generate_students_obj(grades_filename):
     student_obj_list = []
     wb = load_workbook(grades_filename, read_only=True)
     ws = wb['FileFromYedion']
+    rowCounter=0
+
+    global weight_final_project
+    weight_final_project= ws["B3"].value
+
+    global numOfRequiredEx
+    numOfRequiredEx = ws["C3"].value
+
+    Course.weight_final_project = weight_final_project
+    Course.numOfRequiredEx = numOfRequiredEx
+
 
     for row in ws.rows:
+
+        rowCounter +=1
         # Ignore the first two lines
-        if row[0].row < 3:
+        if rowCounter < 9 :
             continue
+        # if row[0].row < 3:
+        #     continue
 
         # Create list of student data [id, lastName, name, grade_1, grade_2]
         curr_student_list = []
         for cell in row:
-            curr_student_list.append(cell.value)
+            if cell.value == "":
+                curr_student_list.append(0)
+            else:
+                curr_student_list.append(cell.value)
 
         # The excel file is from right to left -> reverse
         reversed(curr_student_list)
-        curr_student_obj = Student(curr_student_list)
+        curr_student_obj = Student(curr_student_list,numOfRequiredEx,weight_final_project)
         student_obj_list.append(curr_student_obj)
 
+    wb._archive.close() # close streaming
     return student_obj_list
 
 
@@ -33,7 +61,7 @@ def generate_exemption_dict(exemp_filename):
 
     for row in ws.rows:
         # Ignore the first line
-        if row[0].row == 1:
+        if row[0].row == 1 :
             continue
 
         id = row[0].value
@@ -45,12 +73,45 @@ def generate_exemption_dict(exemp_filename):
     return exemption_dict
 
 
+
+def generate_finalGrades(student_obj_list):
+
+    for student in student_obj_list:
+
+        # get exercise average
+        exGradesList = sorted(student.ex_grade_list, key=int, reverse=True)  # descending order
+        highestGrades = exGradesList[0:student.numOfRequiredEx]  # highest required grades
+        exAvg = sum(highestGrades) / float(len(highestGrades))
+
+        finalGrade = (1 - course.weight_final_project) * exAvg + course.weight_final_project * student.ex_grade_list[-1]
+
+        student.final_grade = finalGrade
+
+
+def gradesToLectureSheet(student_obj_list):
+
+    wb = load_workbook(lectureSheetPath)
+    ws = wb['1']
+    rowCounter = 0
+
+    for row in ws.rows:
+
+        rowCounter +=1
+        # Ignore the first two lines
+        if rowCounter < 15 :
+            continue
+
+        if row[0] in student_obj_list:
+            studentGrade =
+
+
 if __name__ == '__main__':
     course_name = 'עקרונות התכנות בפייתון'
-    course = Course(course_name, 5, 0.4)
-
-    course.student_obj_list = generate_students_obj('/Users/uria/Desktop/Semester b/מבוא לשפת פייתון/תרגילים להגשה/final project/YedionXlsFile_01271_03791.XLSX')
-
-    course.set_exemptions(generate_exemption_dict('/Users/uria/Desktop/Semester b/מבוא לשפת פייתון/תרגילים להגשה/final project/מחר פטורים.xlsx'))
-
+    course = Course(course_name, numOfRequiredEx, weight_final_project)
+    course.student_obj_list = generate_students_obj(gradesFilePath)
+    course.weight_final_project = weight_final_project
+    course.num_ex_required = weight_final_project
+    course.set_exemptions(generate_exemption_dict(exemptionsFilePath))
+    generate_finalGrades(course.student_obj_list)
     course.print_details()
+
